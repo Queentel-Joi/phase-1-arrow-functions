10d66fc4a9b54a3b9dc6e68b49588e7414b62ef9
Queentel <joiqueentel@gmail.com>
2025-06-14 23:57:27 +0300

    creates arrow function

---
+++ index.js
@@ -0,0 +1,13 @@
+let a = 2000;
+let b = 100;
+
+function divide(a,b){
+    console.log(`divide ${a}`)
+     console.log(`divide ${b}`)
+     let answer = a/b;
+     return answer
+}
+
+const square = (a) => a*a;
+
+const add = (a,b) => a+b;
